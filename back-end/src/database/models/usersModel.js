51c6f84c2ffd352e2e0570ab4e86572046bd55a0
Bianca Oura <biancaoura@gmail.com>
2023-02-27 18:28:56 -0300

    feat(model): user association

--- back-end/src/database/models/usersModel.js
+++ back-end/src/database/models/usersModel.js
@@ -1,36 +1,38 @@
 //  Requsito 0
 
 const UsersModel = (sequelize, DataTypes) => {
-    const user = sequelize.define('User', {
+    const User = sequelize.define('User', {
       id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
       },
       name:{
-        type: DataTypes.STRING
+        type: DataTypes.STRING(255)
       },
       email: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING(255)
       },
       password: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING(255)
       },
       role: {
-        type: DataTypes.STRING
+        type: DataTypes.STRING(255)
       }, 
     }, {
-        timestamps:false,
-        underscored:true,
+        timestamps: false,
+        underscored: true,
+        tableName: 'users'
     });
 
-    // user.associate = (models) => {
-    //   user.hasMany(models.BlogPost, {
-    //     foreignKey: 'userId', as: 'blogPosts'
-    //   })
-    // }
+  User.associate = ({ Sale }) => {
+    User.hasMany(Sale,
+      { foreignKey: 'userId', as: 'user' });
+    User.hasMany(Sale,
+      { foreignKey: 'sellerId', as: 'seller' });
+  }   
 
-    return user;
+    return User;
   };
   
   module.exports = UsersModel;
