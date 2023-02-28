30f75d53761a12d9c0f739dda9288fd3c8cfbfe0
andressaribeiroo <andressaclecia.ribeiro@gmail.com>
2023-02-28 19:07:48 -0300

    feat:  Acrescenta funções de remover e adicionar

--- front-end/src/components/ProductQuantity.js
+++ front-end/src/components/ProductQuantity.js
@@ -1,32 +1,85 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-function ProductQuantity({ id }) {
+function ProductQuantity({
+  id,
+  name,
+  price,
+  handleCard,
+  setIsActive,
+}) {
+  const [quantity, setQuantity] = useState(0);
+
+  const updateLocalStorage = (newQuantity) => {
+    const cart = JSON.parse(localStorage.getItem('carrinho'));
+    const arrCartPrepared = cart.filter((item) => item.id !== id);
+    const newItem = {
+      id,
+      name,
+      quantity: newQuantity,
+      unitPrice: price,
+    };
+    localStorage.setItem(
+      'carrinho',
+      JSON.stringify([...arrCartPrepared, newItem]),
+    );
+  };
+
+  const adicionaQuantity = () => {
+    const newQuantity = quantity + 1;
+    setQuantity(newQuantity);
+    updateLocalStorage(newQuantity);
+    setIsActive(false);
+    handleCard(JSON.parse(localStorage.getItem('carrinho')));
+  };
+
+  const removeQuantity = () => {
+    const newQuantity = quantity - 1;
+    if (newQuantity < 0) return setQuantity(0);
+    setQuantity(newQuantity);
+    updateLocalStorage(newQuantity);
+
+    handleCard(JSON.parse(localStorage.getItem('carrinho')));
+  };
+
+  useEffect(() => {
+    localStorage.setItem('carrinho', JSON.stringify([]));
+  }, []);
+
   return (
     <section>
       <button
-             type="button"
-             data-testid={ `customer_products__button-card-rm-item-${id}` }
-             title="remove"
-             onClick={ () =>  }
-           >
-             -
+        type="button"
+        data-testid={ `customer_products__button-card-rm-item-${id}` }
+        title="remove"
+        onClick={ () => removeQuantity }
+      >
+        -
       </button>
 
-    <p
-      data-testid={`customer_products__input-card-quantity-${id}`}>
+      <p
+        data-testid={ `customer_products__input-card-quantity-${id}` }
+      >
         quantidade
-    </p>
+      </p>
 
       <button
-             type="button"
-             data-testid={ `customer_products__button-card-add-item-${id}` }
-             title="add"
-             onClick={ () =>  }
-           >
-             +
-        </button>
+        type="button"
+        data-testid={ `customer_products__button-card-add-item-${id}` }
+        title="add"
+        onClick={ () => adicionaQuantity }
+      >
+        +
+      </button>
     </section>
-  )
+  );
 }
 
 export default ProductQuantity;
+
+ProductQuantity.propTypes = {
+  id: number.isRequired,
+  name: string.isRequired,
+  price: string.isRequired,
+  handleCard: PropTypes.func.isRequired,
+  setIsActive: PropTypes.func.isRequired,
+};
