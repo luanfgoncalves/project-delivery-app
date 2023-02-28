748c32777e1e25f75e94f559066f6d14b1ee4436
luanfgoncalves <luanfgoncalves@outlook.com>
2023-02-28 19:16:50 -0300

    FIX: Corrige erros de lint no backend

--- back-end/src/api/controllers/loginController.js
+++ back-end/src/api/controllers/loginController.js
@@ -4,9 +4,8 @@ const login = async (req, res) => {
   const { email } = req.body;
   const result = await ser.serviceLogin(email);   
   return res.status(200).json(result);
-
 };
 
 module.exports = {
-login
+  login,
 };
