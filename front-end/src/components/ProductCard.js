7eb6390303d402f5b5988e00a705edf2d4f81132
Bianca Oura <biancaoura@gmail.com>
2023-02-28 22:26:51 -0300

    chore: connect component to another

--- front-end/src/components/ProductCard.js
+++ front-end/src/components/ProductCard.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import ProductQuantity from './ProductQuantity';
 
 function ProductCard({ id, urlImage, name, price }) {
+  const [isActive, setIsActive] = useState(true);
+
+  // ainda precisa implementar essa função abaixo e esse useState acima
+  // e colocá-las no ProductQuantity
+  // passei por props daquele jeito ali nas linhas 46/47 mas não sei como vc estava pensando em implementar
+  const handleCard = () => {};
+
+  // coloquei esse useEffect só pra não dar erro no avaliador por causa do isActive
+  useEffect(() => {
+
+  }, [isActive]);
+
   return (
     <>
       <div>
@@ -25,6 +38,14 @@ function ProductCard({ id, urlImage, name, price }) {
         >
           {`R$ ${price}`}
         </p>
+
+        <ProductQuantity
+          id={ id }
+          name={ name }
+          price={ price }
+          handleCard={ handleCard }
+          setIsActive={ setIsActive }
+        />
       </div>
     </>
   );
