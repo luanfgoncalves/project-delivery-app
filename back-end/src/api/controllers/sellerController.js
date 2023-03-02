9165f4a62431a4c5f002eac6383dbf6c01b4d330
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-03-02 13:57:37 -0300

    Fix: Corrigindo erro de Lint SellerController e SellerService

--- back-end/src/api/controllers/sellerController.js
+++ back-end/src/api/controllers/sellerController.js
@@ -1,4 +1,4 @@
-const ser = require('../../database/services/sellerService');
+const ser = require('../services/sellerService');
 
 const getSellers = async (_req, res) => {
   const result = await ser.getSellers();   
--- back-end/src/api/services/sellerService.js
+++ back-end/src/api/services/sellerService.js
@@ -1,10 +1,10 @@
 const { User } = require('../../database/models/index');
 
 const getSeller = async () => {
-  const sellers = await User.findAll({where: {role: 'seller'}});
+  const sellers = await User.findAll({ where: {role: 'seller' }});
   return sellers;
  };
 
 module.exports = {
-  getProducts,
+  getSeller,
 };
