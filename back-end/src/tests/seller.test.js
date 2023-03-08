c03b6a5cb4cdfff0022898a578334c92e33280a8
Bianca Oura <biancaoura@gmail.com>
2023-03-08 00:52:05 -0300

    test(seller): GET all sellers

--- back-end/src/tests/seller.test.js
+++ back-end/src/tests/seller.test.js
@@ -3,26 +3,47 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const app = require('../api/app');
-const { Sale } = require('../database/models/index');
+const { Sale, User } = require('../database/models/index');
 const { newOrder } = require('./mocks/order.mock');
+const { sellerInfo } = require('./mocks/user.mock');
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const sellerOrders = '/seller/orders?seller_id=2';
+const sellerRoute = '/seller';
+
+const sellerOrdersRoute = `${sellerRoute}/orders?seller_id=2`;
 
 describe('Página da pessoa vendedora', () => {
+
+  let response;
+
+  afterEach(sinon.restore);
+
   it('1 - Deve retornar todos os pedidos da pessoa vendedora com o id passado na query', async () => {
     sinon
       .stub(Sale, 'findAll')
       .resolves([newOrder]);
     
-    const response = await chai
+    response = await chai
       .request(app)
-      .get(sellerOrders);
+      .get(sellerOrdersRoute);
 
     expect(response.status).to.be.equal(200);
     expect(response.body).to.deep.equal([newOrder]);
+  });
+
+  it('2 - Deve retornar todas as pessoas vendedoras', async () => {
+    sinon
+      .stub(User, 'findAll')
+      .resolves([sellerInfo]);
+
+    response = await chai
+      .request(app)
+      .get(sellerRoute);
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.deep.equal([sellerInfo]);
   });
 });
